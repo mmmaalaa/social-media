e95b380f10252402864e4913d445579a9f2eb490
mahmoud alaa <lam668956@gmail.com>
2025-05-27 18:19:07 +0300

    fix

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ dotenv.config({ path: "./.env" });
 const app = express();
 const port = process.env.PORT || 3000;
 await bootstrap(app, express);
-app.listen(port, () => {});
+console.log("PORT is:", process.env.PORT);
+console.log("BASE_URL is:", process.env.BASE_URL);
+app.listen(port);
