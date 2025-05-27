360b6755eedd9e72346d37f662d2567931fe274d
mahmoud alaa <lam668956@gmail.com>
2025-05-27 18:19:57 +0300

    fix

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import express from "express";
 import bootstrap from "./src/app.controller.js";
-import dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
 const app = express();
 const port = process.env.PORT || 3000;
 await bootstrap(app, express);
