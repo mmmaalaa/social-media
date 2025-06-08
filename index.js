46f79c2d99e4e965369b82846c29f7fc65182e58
mmmaalaa <mmmaaalaa13@gmail.com>
2025-06-08 14:05:43 +0300

    Remove node_modules and .env from repo and add to .gitignore

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import bootstrap from "./src/app.controller.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 await bootstrap(app, express);
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`app is running on port ${PORT}`);
 });
